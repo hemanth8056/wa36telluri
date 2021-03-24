4441ffd085c2e5cc1d134fbf69ccefbf01c11b2d
hemanth8056 <77633475+hemanth8056@users.noreply.github.com>
2021-03-24 18:59:34 -0500

    new endpoint

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
-var computeRouter = require('./routes/compute')
 
 var app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter);
-app.use('/computation', computeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
